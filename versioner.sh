0ca7d294f056cd5ab95c462621ecd5a29a780481
erramsai <sandeep.yerram5@gmail.com>
2018-05-23 19:47:55 +0530

    Update versioner.sh

--- versioner.sh
+++ versioner.sh
@@ -22,6 +22,7 @@ NEEDS_TAG=`git describe --contains $GIT_COMMIT`
 if [ -z "$NEEDS_TAG" ]; then
     echo "Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
     git tag $NEW_TAG
+    git add .
     git push --tags
 else
     echo "Already a tag on this commit"
